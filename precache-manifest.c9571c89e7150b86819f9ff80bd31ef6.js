75a4f01de6f778900f7dff365b52ae8715dd698b
Kostiantyn Kashinn <kmbit64@gmail.com>
2020-10-28 14:24:55 +0200

    Updates

---
+++ precache-manifest.c9571c89e7150b86819f9ff80bd31ef6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a322abe58ecf41ef77758cf22241a9f3",
+    "url": "/coin-finder/index.html"
+  },
+  {
+    "revision": "f4ed4003370e93f5326e",
+    "url": "/coin-finder/static/css/2.6414bb7b.chunk.css"
+  },
+  {
+    "revision": "8fabd9c971e4856f0cfc",
+    "url": "/coin-finder/static/css/main.d763e10c.chunk.css"
+  },
+  {
+    "revision": "f4ed4003370e93f5326e",
+    "url": "/coin-finder/static/js/2.75d4757b.chunk.js"
+  },
+  {
+    "revision": "44ba416b9a7217ca99c025d9f4dd1124",
+    "url": "/coin-finder/static/js/2.75d4757b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fabd9c971e4856f0cfc",
+    "url": "/coin-finder/static/js/main.f64f77ac.chunk.js"
+  },
+  {
+    "revision": "71e9426ce802f2c629c4",
+    "url": "/coin-finder/static/js/runtime-main.c35107d7.js"
+  }
+]);
